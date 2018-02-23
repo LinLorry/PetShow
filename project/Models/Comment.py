ca321688a8088c0570d6a2feec6351212f1d2cb1
some thing for time <32118370+ycpppppp@users.noreply.github.com>
2018-02-23 20:44:22 +0800

    Update Comment.py

--- project/Models/Comment.py
+++ project/Models/Comment.py
@@ -11,6 +11,8 @@ class Comment(db.Model):
     __user_id = db.Column(db.String(16), nullable=False)
     __to_user_id = db.Column(db.String(16), nullable=False)
     __comment_content = db.Column(db.Text, nullable=False)
+    __comment_time = db.Column(db.String(20),nullable=False)
+    
     #------>发布卡片的部分
     def comment_on_card(self, comment_card_dict):
         '''示例：newcomment_dict{'card_id':'67687',
@@ -22,7 +24,7 @@ class Comment(db.Model):
         self.__user_id = comment_card_dict['user_id']
         self.__to_user_id = comment_card_dict['to_user_id']
         self.__comment_content = comment_card_dict['comment_content']
-
+        self.__comment_time = comment_card_dict['comment_time']
         return True
 
     def check_data(self, user_id, data_dict):
@@ -52,3 +54,17 @@ class Comment(db.Model):
         db.session.delete(comm)
         db.session.commit()
         return True
+    
+    #----->查看某卡片的所有评论
+    def card_all_comment(self,card_id):
+        all = self.query.filter_by(_Comment__card_id=card_id).all()
+        peo = []
+        for one in all:
+            a = User.query.filter_by(_User__id =one.__user_id).first()
+            peo.append({"author":"ome","id":one.__id,"reply_to":one.__to_user_id,"time":one.__comment_time,"content":one.__comment_content})
+        return peo
+    
+    #------>获取某条评论的发布者id
+    def comment_user(self,comment_id):
+        one = self.query.filter_by(_Comment__id=comment_id).first()
+        return one
